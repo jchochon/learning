356e6aca321b1ddf35aa5f983c9fcaafeaf5c9ea
gba <blacko952@gmail.com>
2014-12-07 10:55:55 +0100

    First visual studio git commit

---
+++ c#/MaPremiereApplication/MaPremiereApplication/TrieurDeTableau.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaPremiereApplication
+{
+    class TrieurDeTableau
+    {
+        private delegate void DelegateTri(int[] tableau);
+
+        public void TriAscandant(int [] tableau)
+        {
+            Array.Sort(tableau);
+            foreach (int i in tableau)
+            {
+                Console.WriteLine(i);
+            }
+            
+        }
+        public void TriDescandant(int [] tableau)
+        {
+            Array.Sort(tableau);
+            Array.Reverse(tableau);
+            foreach (int i in tableau)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public void DemoTri(int[] tableau)
+        {
+            DelegateTri tri = TriAscandant;
+            tri += TriDescandant;
+            tri(tableau);
+        }
+
+    }
+}
